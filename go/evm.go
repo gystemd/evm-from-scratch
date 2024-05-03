cf6dacb72c33aae5e76846a5279b5450eb80540f
gystemd <gystemd@gmail.com>
2024-05-03 12:21:25 +0200

    add PUSH operator

--- go/evm.go
+++ go/evm.go
@@ -18,13 +18,27 @@ import (
 func Evm(code []byte) ([]*big.Int, bool) {
 	var stack []*big.Int
 	pc := 0
-
+	// print(len(code))
 	for pc < len(code) {
 		op := code[pc]
 		pc++
 
-		// TODO: Implement the EVM here!
-		_ = op // delete this; it's only here to make the compiler think you're already using `op`
+		switch {
+		case op == 0x00:
+		case op == 0x5f:
+			stack = append([]*big.Int{big.NewInt(0x00)}, stack...)
+		case op > 95 && op < 128:
+			size := int(op) - 95
+			operand := big.NewInt(0)
+			for i := 0; i < size; i++ {
+				/* concatenate the bytes of the operand */
+				operand.Lsh(operand, 8)
+				operand.Or(operand, big.NewInt(int64(code[pc])))
+				pc++
+			}
+			stack = append([]*big.Int{operand}, stack...)
+		default:
+		}
 	}
 
 	return stack, true
